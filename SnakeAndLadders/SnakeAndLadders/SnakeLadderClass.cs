803181a6e7ac11937f1f1316fb8bff07cf62cae6
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-05-14 14:49:46 +0530

    Add : Repeat till player win

--- SnakeAndLadders/SnakeAndLadders/SnakeLadderClass.cs
+++ SnakeAndLadders/SnakeAndLadders/SnakeLadderClass.cs
@@ -13,24 +13,33 @@ namespace SnakeAndLadders
             int position = 0;
 
             Random random = new Random();
-            int dice = random.Next(1, 7);
+            while (position != 100)
+            {
+                int dice = random.Next(1, 7);
+                Console.WriteLine($"The Player rolls a dice and gets: {dice}");
 
-            int option = random.Next(0, 3);
-            if (option == 0)
-            {
-                Console.WriteLine("Player stays at same Position");
+                int option = random.Next(0, 3);
+                if (option == 0)
+                {
+                    Console.WriteLine("Player stays at same Position");
+                }
+                else if (option == 1)
+                {
+                    Console.WriteLine("Player Moves Ahead by {0} from ladder", dice);
+                    position += dice;
+                }
+                else
+                {
+                    Console.WriteLine("Player Moves Behind by {0} from Snake", dice);
+                    position -= dice;
+                }
+                if (position < 0)
+                {
+                    position = 0;
+                }
+                Console.WriteLine("Player Current Position is at : {0}", position);
             }
-            else if (option == 1)
-            {
-                Console.WriteLine("Player Moves Ahead by {0} from ladder", dice);
-                position += dice;
-            }
-            else
-            {
-                Console.WriteLine("Player Moves Behind by {0} from Snake", dice);
-                position -= dice;
-            }
-            Console.WriteLine("Current Position is at : {0}", position);
+            Console.WriteLine("Player is at Position : {0}", position);
         }
     }
 }
