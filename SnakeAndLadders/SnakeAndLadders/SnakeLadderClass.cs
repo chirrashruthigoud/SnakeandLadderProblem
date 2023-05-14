5a198ca051e165e56e62d658ee11823d54eab10b
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-05-14 14:37:08 +0530

    Add : player rolling the dice

--- SnakeAndLadders/SnakeAndLadders/SnakeLadderClass.cs
+++ SnakeAndLadders/SnakeAndLadders/SnakeLadderClass.cs
@@ -10,9 +10,9 @@ namespace SnakeAndLadders
     {
         public void SinglePlayer()
         {
-            int position = 0;
-            Console.WriteLine("Welcome Player 1, Let's start the Game");
-            Console.WriteLine("Player 1 is at position : " + position);
+            Random random = new Random();
+            int dice = random.Next(1, 7);
+            Console.WriteLine($"The Player rolls a dice and gets: {dice}");
         }
     }
 }
