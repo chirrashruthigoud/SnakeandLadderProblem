b61116ca4f90e1dab4ee23184af393f909a8273b
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-05-14 14:28:45 +0530

    Add : single player position

---
+++ SnakeAndLadders/SnakeAndLadders/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadders
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            SnakeLadderClass name=new SnakeLadderClass();
+            name.SinglePlayer();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SnakeAndLadders/SnakeAndLadders/SnakeLadderClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadders
+{
+    public class SnakeLadderClass
+    {
+        public void SinglePlayer()
+        {
+            int position = 0;
+            Console.WriteLine("Welcome Player 1, Let's start the Game");
+            Console.WriteLine("Player 1 is at position : " + position);
+        }
+    }
+}
